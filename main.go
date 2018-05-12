31320d4595c080d2a320e84edb2a3827ae8b16f6
tavish <tavish.gobindram@gmail.com>
2018-05-12 08:55:57 -0400

    implemented latestversions and cmd args parsing

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"encoding/csv"
+	"strings"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/google/go-github/github"
@@ -11,7 +14,14 @@ import (
 // LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
 	var versionSlice []*semver.Version
-	// This is just an example structure of the code, if you implement this interface, the test cases in main_test.go are very easy to run
+	semver.Sort(releases) // sort releases in ascending order
+	for i := len(releases)-1; i >= 0; i-- { // iterate through slice backwards
+		if !releases[i].LessThan(*minVersion) {
+			if i == len(releases)-1 || releases[i].Minor != versionSlice[len(versionSlice)-1].Minor {
+				versionSlice =  append(versionSlice, releases[i])
+			}
+		}
+	}
 	return versionSlice
 }
 
@@ -21,23 +31,39 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 // the correct information, including this line
 func main() {
 	// Github
-	client := github.NewClient(nil)
+	client := github.NewClient(nil) // ':=' serves as both declare and init
 	ctx := context.Background()
 	opt := &github.ListOptions{PerPage: 10}
-	releases, _, err := client.Repositories.ListReleases(ctx, "kubernetes", "kubernetes", opt)
+	file, err := os.Open(os.Args[1])
 	if err != nil {
-		panic(err) // is this really a good way?
+		panic(err)
 	}
-	minVersion := semver.New("1.8.0")
-	allReleases := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		versionString := *release.TagName
-		if versionString[0] == 'v' {
-			versionString = versionString[1:]
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		panic( err)
+	}
+	for i, line := range lines {
+		if i == 0 {
+			// skip header line
+			continue
 		}
-		allReleases[i] = semver.New(versionString)
+		repo := strings.Split(line[0],"/")
+		releases, _, err := client.Repositories.ListReleases(ctx, repo[0], repo[1], opt)
+		if err != nil {
+			fmt.Printf("Could not retrieve releases for '%s'\n", line[0])
+			continue
+		}
+		minVersion := semver.New(line[1])
+		allReleases := make([]*semver.Version, len(releases))
+		for i, release := range releases {
+			versionString := *release.TagName
+			if versionString[0] == 'v' {
+				versionString = versionString[1:]
+			}
+			allReleases[i] = semver.New(versionString)
+		}
+		versionSlice := LatestVersions(allReleases, minVersion)
+		fmt.Printf("latest versions of %s: %s\n", line[0], versionSlice)
 	}
-	versionSlice := LatestVersions(allReleases, minVersion)
-
-	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
 }
