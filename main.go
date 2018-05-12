2737950e83d56d2a4ded702dd0bf058d15829dde
tavish <tavish.gobindram@gmail.com>
2018-05-12 09:10:38 -0400

    added program documentation

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ import (
 // LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
 	var versionSlice []*semver.Version
-	semver.Sort(releases) // sort releases in ascending order
-	for i := len(releases)-1; i >= 0; i-- { // iterate through slice backwards
+	// sort releases in ascending order
+	semver.Sort(releases) 
+	// iterate through slice backwards
+	for i := len(releases)-1; i >= 0; i-- {
+		// release > min version ?
 		if !releases[i].LessThan(*minVersion) {
+			// if first element or element has diff minor version than last item in versionSlice,
+			// then add to versionSlice
 			if i == len(releases)-1 || releases[i].Minor != versionSlice[len(versionSlice)-1].Minor {
 				versionSlice =  append(versionSlice, releases[i])
 			}
@@ -25,42 +30,54 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 	return versionSlice
 }
 
-// Here we implement the basics of communicating with github through the library as well as printing the version
-// You will need to implement LatestVersions function as well as make this application support the file format outlined in the README
-// Please use the format defined by the fmt.Printf line at the bottom, as we will define a passing coding challenge as one that outputs
-// the correct information, including this line
 func main() {
-	// Github
-	client := github.NewClient(nil) // ':=' serves as both declare and init
+	// init Github api reqs
+	client := github.NewClient(nil)
 	ctx := context.Background()
 	opt := &github.ListOptions{PerPage: 10}
+
+	// get filename from cmd args
 	file, err := os.Open(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
+
+	// csv parsing
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
 		panic( err)
 	}
+
 	for i, line := range lines {
+		// skip header line
 		if i == 0 {
-			// skip header line
 			continue
 		}
+
+		// split repositoy field into owner and name components
 		repo := strings.Split(line[0],"/")
+
 		releases, _, err := client.Repositories.ListReleases(ctx, repo[0], repo[1], opt)
+
+		// skip repository if releases could not be retrieved
 		if err != nil {
 			fmt.Printf("Could not retrieve releases for '%s'\n", line[0])
 			continue
 		}
+
 		minVersion := semver.New(line[1])
 		allReleases := make([]*semver.Version, len(releases))
+
 		for i, release := range releases {
 			versionString := *release.TagName
+			
+			// remove preceeding 'v' from version strings
 			if versionString[0] == 'v' {
 				versionString = versionString[1:]
 			}
+
+			// store version string as a semver struct
 			allReleases[i] = semver.New(versionString)
 		}
 		versionSlice := LatestVersions(allReleases, minVersion)
